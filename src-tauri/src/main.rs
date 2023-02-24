bc3694f85660dd0cbd7afd773cc38b42e6f2b327
Adam Rohdin <adam.rohdin@liu.se>
2023-02-24 17:53:34 +0100

    intial testing with tauri

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,9 +9,25 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn start_ffmpeg() {
+  let code = "$dir = ($HOME + '/Videos'); New-Item ($dir + '/working.txt'); echo Y | ffmpeg -framerate 60 -i 'C:/OpenSpace/DATA/user/screenshots/2023-02-23-14-36/OpenSpace_%06d.png' -c:v libx264 -pix_fmt yuv420p -crf 23 $HOME/Videos/output.mp4 -hide_banner 1>$HOME/Videos/output-log.txt 2>&1; New-Item ($dir + '/done.txt')";
+  std::process::Command::new("powershell").args(["-command", code]).spawn();
+}
+
+//return $VIDEO
+#[tauri::command]
+fn getAbsVideoPath() {
+    // https://crates.io/crates/directories 
+}
+
+// Todo:
+// create a function that check if ffmpeg log-file exists, and if it does, parse it and try to figuire out how far the rendering has come.
+// to prevent mal-reading of file due to in-progress line writing, always remove the latest line and then check for "frame= ".
+
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  tauri::Builder::default()
+      .invoke_handler(tauri::generate_handler![start_ffmpeg, greet])
+      .run(tauri::generate_context!())
+      .expect("error while running tauri application");
 }
--- src/main.js
+++ src/main.js
@@ -1,12 +1,8 @@
 const { invoke } = window.__TAURI__.tauri;
+const { readDir, BaseDirectory } = window.__TAURI__.fs;
 
 let greetInputEl;
 let greetMsgEl;
-
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
-}
 
 window.addEventListener("DOMContentLoaded", () => {
   greetInputEl = document.querySelector("#greet-input");
@@ -15,3 +11,85 @@ window.addEventListener("DOMContentLoaded", () => {
     .querySelector("#greet-button")
     .addEventListener("click", () => greet());
 });
+
+async function greet() {
+  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+}
+
+//DEFAULTS
+DEFAULT_VIDEO_PATH = "" 
+
+
+//rust function test
+async function start_ffmpeg() {
+  return await invoke("start_ffmpeg");
+}
+
+//helper function to connect to opensapce
+let connectToOpenSpace = () => {
+  //setup the api params
+  var host = document.getElementById('ipaddress').value;
+  var api = window.openspaceApi(host, 4682);
+  //notify users on disconnect
+  api.onDisconnect(() => {
+    console.log("disconnected");
+    document.getElementById('container').className = "disconnected";
+    document.getElementById('connection-status').style.opacity = 1;
+    var disconnectedString = "Connect to OpenSpace: ";
+    disconnectedString += '<input id="ipaddress" type=text placeholder="Enter ip address" /> ';
+    disconnectedString += '<button onClick="connectToOpenSpace();">Connect</button>';
+    document.getElementById('connection-status').innerHTML = disconnectedString;
+    openspace = null;
+  });
+  //notify users and map buttons when connected
+  api.onConnect(async () => {
+    try {
+      document.getElementById('container').className = "connected";
+      document.getElementById('connection-status').innerHTML = "Connected to OpenSpace";
+      openspace = await api.library();
+      console.log('connected');
+
+      setScreenshotPathText();
+      setVideoExportPathText();
+
+    } catch (e) {
+      console.log('OpenSpace library could not be loaded: Error: \n', e);
+      return;
+    }
+  })
+  //connect
+  api.connect();
+};
+
+async function setScreenshotPathText() {
+  let path = await getFolderPath("${SCREENSHOTS}");
+  document.getElementById("screenshotfolder").innerText = "Current Screenshot folder: " + path[1];
+}
+
+async function setVideoExportPathText() {
+  // https://crates.io/crates/directories ?
+
+  // const search = '\\';
+  // const replaceWith = '/';
+  // how to THE_STRING.split(search).join(replaceWith);
+
+  var r = await readDir("",{ dir: BaseDirectory.Home});
+  r.forEach((item) => {
+    if(item.name === "Videos") {
+      document.getElementById("videofolder").innerText = "Current Screenshot folder: " + item.path;
+    }
+  })
+}
+
+async function setScreenshotFolder() {
+  // prob. not needed
+}
+
+async function getFolderPath(relative){
+  return await openspace.absPath(relative);
+}
+
+async function dirTest() {
+
+}
