ea5574974d5f9ec9ea6750fc30efd0e8c2025edb
Adam Rohdin <adam.rohdin@liu.se>
2023-02-28 08:55:33 +0100

    first working draft

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -10,9 +10,31 @@ fn greet(name: &str) -> String {
 }
 
 #[tauri::command]
-fn start_ffmpeg() {
-  let code = "$dir = ($HOME + '/Videos'); New-Item ($dir + '/working.txt'); echo Y | ffmpeg -framerate 60 -i 'C:/OpenSpace/DATA/user/screenshots/2023-02-23-14-36/OpenSpace_%06d.png' -c:v libx264 -pix_fmt yuv420p -crf 23 $HOME/Videos/output.mp4 -hide_banner 1>$HOME/Videos/output-log.txt 2>&1; New-Item ($dir + '/done.txt')";
-  std::process::Command::new("powershell").args(["-command", code]).spawn();
+async fn pre_ffmpeg() -> String {
+  let create_indication_file = "New-Item $HOME/Videos/rendering.space";
+  let out_cif = std::process::Command::new("powershell").args(["-command", create_indication_file]).output().expect("Error running create_indication_file");
+  return (out_cif.status).to_string();
+}
+
+#[tauri::command]
+async fn start_ffmpeg() -> Vec<String> {
+
+  let ffmpeg_code = "(echo Y | ffmpeg -framerate 60 -i 'C:/OpenSpace/DATA/user/screenshots/2023-02-23-14-36/OpenSpace_%06d.png' -c:v libx264 -pix_fmt yuv420p -crf 23 $HOME/Videos/output.mp4 -hide_banner 1> $HOME/Videos/progress.space 2>&1)";
+  let remove_indication_file = "Remove-Item $HOME/Videos/rendering.space";
+
+  let out_fc = std::process::Command::new("powershell").args(["-command", ffmpeg_code]).output().expect("Error running ffmpeg_code");
+  let put_rif = std::process::Command::new("powershell").args(["-command", remove_indication_file]).output().expect("Error running remove_indication_file");
+
+  let mut v : Vec<String> = Vec::new();
+  let mut vec = vec![(out_fc.status).to_string(), (put_rif.status).to_string()];
+
+  return vec;
+}
+
+#[tauri::command]
+async fn check_progress() -> bool{
+  let b = std::path::Path::new("C:/Users/Adam/Videos/rendering.space").exists();
+  return b;
 }
 
 //return $VIDEO
@@ -27,7 +49,7 @@ fn getAbsVideoPath() {
 
 fn main() {
   tauri::Builder::default()
-      .invoke_handler(tauri::generate_handler![start_ffmpeg, greet])
+      .invoke_handler(tauri::generate_handler![start_ffmpeg, pre_ffmpeg, check_progress, greet])
       .run(tauri::generate_context!())
       .expect("error while running tauri application");
 }
--- src/main.js
+++ src/main.js
@@ -4,14 +4,6 @@ const { readDir, BaseDirectory } = window.__TAURI__.fs;
 let greetInputEl;
 let greetMsgEl;
 
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document
-    .querySelector("#greet-button")
-    .addEventListener("click", () => greet());
-});
-
 async function greet() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
   greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
@@ -23,7 +15,48 @@ DEFAULT_VIDEO_PATH = ""
 
 //rust function test
 async function start_ffmpeg() {
-  return await invoke("start_ffmpeg");
+  let pre_status = await invoke("pre_ffmpeg").then( (res) => {return res});
+  console.log("pre status: " + pre_status);
+  if(pre_status.includes(0)) {
+    check_progress();
+    //render status
+    let m = await invoke("start_ffmpeg").then( (msg) => {return msg});
+    console.log(m);
+  }
+  else {
+    //delete the progress.space file
+    console.log("Error: Something went wrong in pre_ffmpeg");
+    return;
+  }
+
+
+
+  // check if success and return false/true depending on result
+}
+
+async function check_progress() {
+
+  document.getElementById('container').className = "rendering";
+  document.getElementById('connection-status').innerHTML = "Rendering...";
+
+  let fname = async function() {
+    let b = await invoke("check_progress").then( (msg) => {return msg});
+    console.log(b);
+    if(!b) {
+      clearInterval(_INTERLVA_ID_);
+      setReadyState();
+    }    
+  }
+
+  _INTERLVA_ID_ = setInterval(fname, 500);
+
+}
+
+async function test() {
+
+  let res = await invoke("check_progress").then( (msg) => {return msg});
+  console.log("is in progress: " + res);
+
 }
 
 //helper function to connect to opensapce
@@ -33,23 +66,15 @@ let connectToOpenSpace = () => {
   var api = window.openspaceApi(host, 4682);
   //notify users on disconnect
   api.onDisconnect(() => {
-    console.log("disconnected");
-    document.getElementById('container').className = "disconnected";
-    document.getElementById('connection-status').style.opacity = 1;
-    var disconnectedString = "Connect to OpenSpace: ";
-    disconnectedString += '<input id="ipaddress" type=text placeholder="Enter ip address" /> ';
-    disconnectedString += '<button onClick="connectToOpenSpace();">Connect</button>';
-    document.getElementById('connection-status').innerHTML = disconnectedString;
+    setDisconnectedState();
     openspace = null;
   });
   //notify users and map buttons when connected
   api.onConnect(async () => {
     try {
-      document.getElementById('container').className = "connected";
-      document.getElementById('connection-status').innerHTML = "Connected to OpenSpace";
+      setReadyState();
       openspace = await api.library();
-      console.log('connected');
-
+      
       setScreenshotPathText();
       setVideoExportPathText();
 
@@ -77,7 +102,7 @@ async function setVideoExportPathText() {
   var r = await readDir("",{ dir: BaseDirectory.Home});
   r.forEach((item) => {
     if(item.name === "Videos") {
-      document.getElementById("videofolder").innerText = "Current Screenshot folder: " + item.path;
+      document.getElementById("videofolder").innerText = "Current video export folder: " + item.path;
     }
   })
 }
@@ -92,4 +117,24 @@ async function getFolderPath(relative){
 
 async function dirTest() {
 
+}
+
+function setReadyState() {
+  console.log('connected');
+  document.getElementById('container').className = "connected";
+  document.getElementById('connection-status').innerHTML = "Ready";
+}
+
+function setRenderingState() {
+
+}
+
+function setDisconnectedState() {
+  console.log("disconnected");
+  document.getElementById('container').className = "disconnected";
+  document.getElementById('connection-status').style.opacity = 1;
+  var disconnectedString = "Connect to OpenSpace: ";
+  disconnectedString += '<input id="ipaddress" type=text placeholder="Enter ip address" /> ';
+  disconnectedString += '<button onClick="connectToOpenSpace();">Connect</button>';
+  document.getElementById('connection-status').innerHTML = disconnectedString;
 }
