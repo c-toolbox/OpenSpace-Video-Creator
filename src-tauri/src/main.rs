b36eb03d904218ece59772b850b369548016724b
Adam Rohdin <adam.rohdin@liu.se>
2023-03-02 17:49:16 +0100

    Mostly feature complete

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,44 +3,166 @@
     windows_subsystem = "windows"
 )]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+// ^^^^^^ Must be first ^^^^^^
+
+// Code goes below
 
 #[tauri::command]
 async fn pre_ffmpeg() -> String {
-  let create_indication_file = "New-Item $HOME/Videos/rendering.space";
+  let create_indication_file = "New-Item $Env:temp/rendering.space -Force";
   let out_cif = std::process::Command::new("powershell").args(["-command", create_indication_file]).output().expect("Error running create_indication_file");
   return (out_cif.status).to_string();
 }
 
 #[tauri::command]
-async fn start_ffmpeg() -> Vec<String> {
+async fn start_ffmpeg(screenshotspath: String, filename: String) -> Vec<String> {
 
-  let ffmpeg_code = "(echo Y | ffmpeg -framerate 60 -i 'C:/OpenSpace/DATA/user/screenshots/2023-02-23-14-36/OpenSpace_%06d.png' -c:v libx264 -pix_fmt yuv420p -crf 23 $HOME/Videos/output.mp4 -hide_banner 1> $HOME/Videos/progress.space 2>&1)";
-  let remove_indication_file = "Remove-Item $HOME/Videos/rendering.space";
 
-  let out_fc = std::process::Command::new("powershell").args(["-command", ffmpeg_code]).output().expect("Error running ffmpeg_code");
-  let put_rif = std::process::Command::new("powershell").args(["-command", remove_indication_file]).output().expect("Error running remove_indication_file");
+  let path = get_latest_directory(screenshotspath);
 
-  let mut v : Vec<String> = Vec::new();
-  let mut vec = vec![(out_fc.status).to_string(), (put_rif.status).to_string()];
+  //make dynamic output name (Adam_OpenSpace_2023-03-14.mp4)
+  let ffmpeg_code = "$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'; (echo Y | ffmpeg -framerate 60 -i '".to_owned() + &path + "/OpenSpace_%06d.png' -c:v libx264 -pix_fmt yuv420p -s 1920x1080 -crf 23 $HOME/Videos/" + &filename + " -hide_banner 1> $Env:temp/progress.space 2>&1)";
+  
+  
 
+  let out_fc = std::process::Command::new("powershell").args(["-command", &ffmpeg_code]).output().expect("Error running ffmpeg_code");
+  
+
+  let vec = vec![(out_fc.status).to_string()];
+
+  return vec;
+
+}
+
+#[tauri::command]
+async fn post_ffmpeg() -> Vec<String> {
+  let remove_indication_file = "Remove-Item $Env:temp/rendering.space";
+  let remove_progress_file = "Remove-Item $Env:temp/progress.space";
+  
+  let out_pif = std::process::Command::new("powershell").args(["-command", remove_progress_file]).output().expect("Error running remove_progress_file");
+  let out_rif = std::process::Command::new("powershell").args(["-command", remove_indication_file]).output().expect("Error running remove_indication_file");
+  
+  let vec = vec![(out_pif.status).to_string(), (out_rif.status).to_string()];
   return vec;
 }
 
 #[tauri::command]
-async fn check_progress() -> bool{
-  let b = std::path::Path::new("C:/Users/Adam/Videos/rendering.space").exists();
+async fn check_if_rendering(videopath: String) -> bool{
+  let fullpath = std::env::temp_dir().to_string_lossy().into_owned() + "rendering.space";
+  let b = std::path::Path::new(&fullpath).exists();
   return b;
 }
 
-//return $VIDEO
+fn get_latest_directory(path: String) -> String {
+  let mut map: std::collections::HashMap<String, std::time::Duration> = std::collections::HashMap::new();
+  let mut retpath = String::new();
+
+  if path.to_lowercase().contains("screenshots") && path.to_lowercase().contains("openspace") {
+    if let Ok(entries) = std::fs::read_dir(&path) {
+      for entry in entries {
+        if let Ok(entry) = entry {
+          if let Ok(metadata) = entry.metadata() {
+            map.insert(String::from(entry.path().display().to_string()), metadata.modified().unwrap().elapsed().unwrap());
+          }
+        }
+      }
+    }
+  
+    let mut rettime: std::time::Duration = std::time::Duration::from_secs(std::u64::MAX);
+  
+    for(k,v) in map.iter() {
+      if rettime > *v {
+        rettime = *v;
+        retpath = (&k).to_string();
+      }
+    }
+  }
+
+  return retpath;
+}
+
+
 #[tauri::command]
-fn getAbsVideoPath() {
-    // https://crates.io/crates/directories 
+async fn check_progress(path: String) -> String{
+
+  let progress_file = std::env::temp_dir().to_string_lossy().into_owned() + "progress.space";
+
+  let contents = std::fs::read_to_string(progress_file)
+        .expect("Should have been able to read the file");
+
+  return contents;
+}
+
+
+#[tauri::command]
+async fn get_frame_count(path: String) -> u32 {
+  let screenshot_folder_path = get_latest_directory(path);
+  let paths = std::fs::read_dir(screenshot_folder_path).unwrap();
+  return paths.count().try_into().unwrap();
+}
+
+
+#[tauri::command]
+async fn open_fs(path: String) {
+  std::process::Command::new( "explorer" )
+  .arg( path )
+  .spawn( )
+  .unwrap( );
+}
+
+
+#[tauri::command]
+async fn get_latest_recording(path: String) -> String{
+  let mut map: std::collections::HashMap<String, std::time::Duration> = std::collections::HashMap::new();
+
+  if let Ok(entries) = std::fs::read_dir(&path) {
+    for entry in entries {
+      if let Ok(entry) = entry {
+        if let Ok(metadata) = entry.metadata() {
+          let p = String::from(entry.path().display().to_string());
+          let d = metadata.modified().unwrap().elapsed().unwrap();
+          if p.contains(".osrec") {
+            map.insert(p, d);
+          }
+        }
+      }
+    }
+  }
+
+  let mut retpath = String::new();
+  let mut rettime: std::time::Duration = std::time::Duration::from_secs(std::u64::MAX);
+
+  for(k,v) in map.iter() {
+    if rettime > *v {
+      rettime = *v;
+      retpath = (&k).to_string();
+    }
+  }
+
+  return retpath;
+}
+
+#[tauri::command]
+async fn nuke_screenshots_folder(screenshotfolderpath: String) {
+
+  let currentfolder = get_latest_directory(screenshotfolderpath);
+  let mut counter = 0;
+
+  if let Ok(entries) = std::fs::read_dir(currentfolder) {
+    for entry in entries {
+      if let Ok(entry) = entry {
+        let filename = entry.path().file_name().unwrap().to_string_lossy().into_owned();
+        if filename.contains("OpenSpace_") && filename.contains(".png") {
+          if let Ok(res) = std::fs::remove_file(entry.path()) {
+            counter += 1;
+          }
+        }
+      }
+    }
+  }
+
+  println!("Deleted {} number of files", counter);
+
 }
 
 // Todo:
@@ -49,7 +171,12 @@ fn getAbsVideoPath() {
 
 fn main() {
   tauri::Builder::default()
-      .invoke_handler(tauri::generate_handler![start_ffmpeg, pre_ffmpeg, check_progress, greet])
+      .invoke_handler(tauri::generate_handler!
+        [
+          start_ffmpeg, pre_ffmpeg, check_if_rendering, check_progress, 
+          get_frame_count, open_fs, get_latest_recording, nuke_screenshots_folder,
+          post_ffmpeg
+        ])
       .run(tauri::generate_context!())
       .expect("error while running tauri application");
 }
--- src/main.js
+++ src/main.js
@@ -1,82 +1,36 @@
 const { invoke } = window.__TAURI__.tauri;
 const { readDir, BaseDirectory } = window.__TAURI__.fs;
+const { open } = window.__TAURI__.dialog
 
-let greetInputEl;
-let greetMsgEl;
-
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
-}
-
-//DEFAULTS
-DEFAULT_VIDEO_PATH = "" 
-
-
-//rust function test
-async function start_ffmpeg() {
-  let pre_status = await invoke("pre_ffmpeg").then( (res) => {return res});
-  console.log("pre status: " + pre_status);
-  if(pre_status.includes(0)) {
-    check_progress();
-    //render status
-    let m = await invoke("start_ffmpeg").then( (msg) => {return msg});
-    console.log(m);
-  }
-  else {
-    //delete the progress.space file
-    console.log("Error: Something went wrong in pre_ffmpeg");
-    return;
-  }
+_FILE_NAME_ = "";
+_INTERVAL_PROGRESS_ = null;
+_INTERVAL_FRAMES_ = null;
+_SCREENSHOTS_PATH_ = "";
+_VIDEO_PATH_ = "";
+_RECORDINGS_PATH_ = "";
+_ANIMALS_ = null;
+openspace = null;
 
 
-
-  // check if success and return false/true depending on result
-}
-
-async function check_progress() {
-
-  document.getElementById('container').className = "rendering";
-  document.getElementById('connection-status').innerHTML = "Rendering...";
-
-  let fname = async function() {
-    let b = await invoke("check_progress").then( (msg) => {return msg});
-    console.log(b);
-    if(!b) {
-      clearInterval(_INTERLVA_ID_);
-      setReadyState();
-    }    
-  }
-
-  _INTERLVA_ID_ = setInterval(fname, 500);
-
-}
-
-async function test() {
-
-  let res = await invoke("check_progress").then( (msg) => {return msg});
-  console.log("is in progress: " + res);
-
-}
+const ASSERT_INPUT = () => { throw new Error('param is required'); };
 
 //helper function to connect to opensapce
 let connectToOpenSpace = () => {
   //setup the api params
-  var host = document.getElementById('ipaddress').value;
-  var api = window.openspaceApi(host, 4682);
+  var api = window.openspaceApi(null, 4682);
   //notify users on disconnect
   api.onDisconnect(() => {
-    setDisconnectedState();
+    set_disconnected_state();
     openspace = null;
   });
   //notify users and map buttons when connected
   api.onConnect(async () => {
     try {
-      setReadyState();
+      set_ready_state();
       openspace = await api.library();
       
-      setScreenshotPathText();
-      setVideoExportPathText();
+      set_screenshot_path();
+      set_video_export_path();
 
     } catch (e) {
       console.log('OpenSpace library could not be loaded: Error: \n', e);
@@ -87,54 +41,262 @@ let connectToOpenSpace = () => {
   api.connect();
 };
 
-async function setScreenshotPathText() {
-  let path = await getFolderPath("${SCREENSHOTS}");
-  document.getElementById("screenshotfolder").innerText = "Current Screenshot folder: " + path[1];
+async function reconnect() {
+  connectToOpenSpace();
 }
 
-async function setVideoExportPathText() {
-  // https://crates.io/crates/directories ?
 
-  // const search = '\\';
-  // const replaceWith = '/';
-  // how to THE_STRING.split(search).join(replaceWith);
+async function start_ffmpeg() {
 
+  document.getElementById("ffmpeg_button").disabled = true;
+
+  let pre_status = await invoke("pre_ffmpeg").then( (res) => {return res});
+  console.log("pre status: " + pre_status);
+
+  if(pre_status.includes(0)) {
+
+    let name = (document.getElementById("username").value).replace(/\s/g,'').replace(/[^a-z]/gi, '');
+    name = (name) ? name : "Astronaut";
+    let animalSuffix = get_animal_suffix();
+    _FILE_NAME_ = name_builder(name, animalSuffix);
+    
+    //render status
+    check_progress();
+    console.log(_SCREENSHOTS_PATH_);
+    let m = await invoke("start_ffmpeg", {screenshotspath: _SCREENSHOTS_PATH_, filename: _FILE_NAME_}).then( (msg) => {return msg});
+    console.log(m);
+  }
+  else {
+    //delete the progress.space file
+    console.log("Error: Something went wrong in pre_ffmpeg");
+    return;
+  }
+  //maybe:
+  // check if success and return false/true depending on result
+}
+
+async function check_progress() {
+
+  document.getElementById('container').classList.add("rendering");
+  document.getElementById('connection-status').innerHTML = "Renderar video (0%) 🚀";
+
+  let frame_count = await count_frames();
+
+  let fname = async function() {
+    let b = await invoke("check_if_rendering", {videopath: _VIDEO_PATH_}).then( (msg) => {return msg});
+    
+    let s = await invoke("check_progress", {path: "C:/Users/Adam/Videos"});
+    s = s.split(/[\n]/).filter( line => line.includes("frame="));
+    s = s.map( x => (x.replace(/\s/g,'')).split("fps=")[0].replace(/[^0-9]/gi,'') );
+
+    if(s.length >= 1) {
+      let percent = s[s.length - 1] / frame_count;
+      let prog = parseFloat((percent * 100).toPrecision(2)) + "%";
+
+      document.getElementById('connection-status').innerHTML = "Renderar video (" + prog + ") 🚀";
+
+      if(percent === 1) {
+        let r = await invoke("post_ffmpeg");
+        console.log(r);
+        
+        clearInterval(_INTERVAL_PROGRESS_);
+        clear_state();
+
+        let d = new Date();
+        let p = document.createElement("p");
+        document.getElementById('messages').prepend(d.toTimeString().split(' ')[0] + " - filnamn: " + _FILE_NAME_, p);
+        document.getElementById("ffmpeg_button").disabled = false;
+      }
+    }   
+  }
+
+  _INTERVAL_PROGRESS_ = setInterval(fname, 250);
+
+}
+
+async function set_screenshot_path() {
+  _SCREENSHOTS_PATH_ = await get_screenshot_folder_path();
+  console.log(_SCREENSHOTS_PATH_);
+}
+
+async function set_video_export_path() {
   var r = await readDir("",{ dir: BaseDirectory.Home});
   r.forEach((item) => {
     if(item.name === "Videos") {
-      document.getElementById("videofolder").innerText = "Current video export folder: " + item.path;
+      _VIDEO_PATH_ = item.path;
+      console.log(_VIDEO_PATH_);
     }
   })
 }
 
-async function setScreenshotFolder() {
-  // prob. not needed
+function format_path(path) {
+  return path.replaceAll("\\", "/");
 }
 
-async function getFolderPath(relative){
-  return await openspace.absPath(relative);
+async function get_screenshot_folder_path(){
+  let ret = await openspace.absPath("${SCREENSHOTS}");
+  let rev = ret[1].split("\\");
+  let str = "";
+
+  for(let i = 0; i < rev.length-1; ++i) {
+    if(i != rev.length-2) {
+      str += rev[i] + "\\";
+      continue;
+    }
+    str += rev[i];
+  }
+
+  console.log(str);
+
+  return str;
 }
 
-async function dirTest() {
-
-}
-
-function setReadyState() {
+function set_ready_state() {
   console.log('connected');
   document.getElementById('container').className = "connected";
-  document.getElementById('connection-status').innerHTML = "Ready";
+  document.getElementById('connection-status').innerHTML = "Redo att köra 🛰️";
+
+  document.getElementById('isDisconnected').style.display = "none";
+  document.getElementById('isConnected').style.display = "block";
+
+  document.getElementById("startrecording").style.display = "block";
+  document.getElementById("stoprecording").style.display = "none";
 }
 
-function setRenderingState() {
-
-}
-
-function setDisconnectedState() {
+function set_disconnected_state() {
   console.log("disconnected");
   document.getElementById('container').className = "disconnected";
   document.getElementById('connection-status').style.opacity = 1;
-  var disconnectedString = "Connect to OpenSpace: ";
-  disconnectedString += '<input id="ipaddress" type=text placeholder="Enter ip address" /> ';
-  disconnectedString += '<button onClick="connectToOpenSpace();">Connect</button>';
-  document.getElementById('connection-status').innerHTML = disconnectedString;
+  document.getElementById('connection-status').innerHTML = "Du måste starta OpenSpace 🔧";
+  
+  document.getElementById('isDisconnected').style.display = "block";
+  document.getElementById('isConnected').style.display = "none";
+}
+
+function set_recording_state() {
+  document.getElementById('container').classList.add("recording");
+  document.getElementById('connection-status').innerHTML = "Inspelning pågår 🎥";
+}
+
+function clear_state() {
+
+  //not good
+  document.getElementById('connection-status').classList.remove("recording");
+
+  if(openspace == null) {
+    set_disconnected_state();
+  }
+
+  if(openspace) {
+    set_ready_state();
+  }
+}
+
+
+async function count_frames() {
+  let frame_count = await invoke("get_frame_count", {path: _SCREENSHOTS_PATH_});
+  return frame_count;
+}
+
+function get_animal_suffix(){
+  let num = get_randon_number(_ANIMALS_.length);
+  let animal = (_ANIMALS_[num].name).replace(/\s/g,'').replace(/[^0-9a-z]/gi, '');
+  animal += get_randon_number(1000); //0000 - 9999 
+  console.log(animal);
+  return animal;
+}
+
+async function load_animals() {
+  _ANIMALS_ = await fetch("./animal-names.json")
+.then(response => {
+   return response.json();
+});
+}
+
+function get_randon_number(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function name_builder(username, animalsuffix) {
+  return username + "_OpenSpace_" + animalsuffix + ".mp4";
+}
+
+async function set_openspace_ui_state(state) {
+  await openspace.setPropertyValueSingle("RenderEngine.ShowVersion", state);
+  await openspace.setPropertyValueSingle("RenderEngine.ShowCamera", state);
+  await openspace.setPropertyValueSingle("RenderEngine.ShowLog", state);
+  await openspace.setPropertyValueSingle("Dashboard.IsEnabled", state);
+  await openspace.setPropertyValueSingle("Modules.CefWebGui.Visible", state);
+}
+
+async function start_recording() {
+  document.getElementById("startrecording").style.display = "none";
+  document.getElementById("stoprecording").style.display = "block";
+
+  filename = get_animal_suffix();
+
+  set_recording_state();
+
+  await openspace.sessionRecording.startRecording(filename)
+}
+
+async function stop_recording() {
+  await openspace.sessionRecording.stopRecording()
+  clear_state();
+  document.getElementById("startrecording").style.display = "block";
+  document.getElementById("stoprecording").style.display = "none";
+}
+
+function open_fs(path) {
+  invoke("open_fs", {path: path});
+}
+
+function open_video_folder() {
+  open_fs(_VIDEO_PATH_);
+}
+
+function open_screenshots_folder() {
+  open_fs(_SCREENSHOTS_PATH_);
+}
+
+async function get_latest_recording(path) {
+  return await invoke("get_latest_recording", {path: path});
+}
+
+async function generate_frames() {
+
+  document.getElementById('container').classList.add("generating");
+  document.getElementById('connection-status').innerHTML = "Skapar bildsekvens 🎞️";
+
+  _RECORDINGS_PATH_ = (await openspace.absPath("${RECORDINGS}"))[1];
+
+  await openspace.sessionRecording.enableTakeScreenShotDuringPlayback(60);
+
+  await set_openspace_ui_state(false);
+
+  //nuke folder
+  await invoke("nuke_screenshots_folder", {screenshotfolderpath: _SCREENSHOTS_PATH_});
+
+  //reset counter
+  await openspace.resetScreenshotNumber();
+
+  //render
+  let filename = await get_latest_recording(_RECORDINGS_PATH_);
+  await openspace.sessionRecording.startPlayback(filename,false);
+
+  let looper = async function() {
+    let isplaying = (await openspace.sessionRecording.isPlayingBack())[1];
+    console.log("playback!");
+    if (!isplaying) {
+      clearInterval(_INTERVAL_FRAMES_);
+      await set_openspace_ui_state(true);
+      clear_state();
+    }
+  }
+
+  _INTERVAL_FRAMES_ = await setInterval(looper, 250);
+}
+
+async function create_video() {
+
 }
